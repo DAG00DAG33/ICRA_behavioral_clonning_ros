27d5a16917f75b294bb1bfa64b9ba3ecc96613f6
Dominic Nightingale <dag00dag33@gmail.com>
2022-05-24 03:08:17 +0000

    init

---
+++ behavioral_cloning_ws/src/data_acquisition/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'data_acquisition'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='djnighti@ucsd.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'data_acquisition_node = data_acquisition.data_acquisition_node:main'
+        ],
+    },
+)
